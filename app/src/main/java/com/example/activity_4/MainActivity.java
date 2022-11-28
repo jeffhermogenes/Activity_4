c07f304e354872adfcf9ff5881fd2cd2c2d4a59a
unknown <gerardjefferson.hermogenes.cics@ust.edu.ph>
2022-11-28 08:12:02 +0800

    initial commit

--- app/src/main/java/com/example/activity_4/MainActivity.java
+++ app/src/main/java/com/example/activity_4/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.activity_4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,22 +25,52 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void insertRecord (){
-        FirebaseDatabase.getInstance().getReference()
+    public void insertRecord(View v){
+        String id = editID.getText().toString();
+        String name = editName.getText().toString();
+        String section = editSection.getText().toString();
+        Student student = new Student(name, section);
+        dbRef.child(id).setValue(student);
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+    }
+    public void showData(View v){
+        String id = editID.getText().toString();
+        DatabaseReference dbDoc = dbRef.child(id);
+        ValueEventListener veListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot ds) {
+                Student student = ds.getValue(Student.class);
+                editName.setText(student.getName());
+                editSection.setText(student.getSection());
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        dbDoc.addValueEventListener(veListener);
     }
 
-
-    public void showData () {
-
+    public void updateRecord(View v){
+        String id = editID.getText().toString();
+        String name = editName.getText().toString();
+        String section = editSection.getText().toString();
+        HashMap<String, Object> studMap= new HashMap <>();
+        studMap.put("name",name);
+        studMap.put("section",section);
+        dbRef.child(id).updateChildren(studMap).addOnSuccessListener(suc -> {
+            Toast.makeText(this, "record updated...", Toast.LENGTH_SHORT).show();
+        }).addOnFailureListener(err -> {
+            Toast.makeText(this, err.getMessage(), Toast.LENGTH_SHORT).show();
+        });;
     }
 
-    public void updateRecord () {
-
+    public void deleteRecord(View v){
+        String id = editID.getText().toString();
+        dbRef.child(id).removeValue().addOnSuccessListener(suc -> {
+            Toast.makeText(this, "record deleted...", Toast.LENGTH_SHORT).show();
+        }).addOnFailureListener(err -> {
+            Toast.makeText(this, err.getMessage(), Toast.LENGTH_SHORT).show();
+        });
     }
-
-    public void deleteRecord () {
-
-    }
-
-}
